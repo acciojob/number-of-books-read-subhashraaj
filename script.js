4b25698995ae717a451371e5fea77c080b29f983
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-11 19:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,9 +17,13 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+	let count = 0;
+	for (let i=0;i<library.length;i++){
+		if(library[i].readingStatus) count++;
+	}
+	return count;
 };
 
 // Do not change the code below
 
-alert(numberOfBooksRead());
+//alert(numberOfBooksRead());
